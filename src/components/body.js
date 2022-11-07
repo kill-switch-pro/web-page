1b712cb178bdf06de08f928e24a6918b7e8c3ee0
dev S <stephensamuel063@gmail.com>
2022-11-07 21:53:23 +0100

    feat : added new feature

--- src/components/body.js
+++ src/components/body.js
@@ -1,4 +1,3 @@
-
 import github from "../assets/github.png";
 //import I4G from "../assets/I4G.png";
 //import zuri from "../assets/Zuri-internship.png";
@@ -14,37 +13,35 @@ function Body() {
   const zuriDesign = "https://books.zuri.team/design-rules";
 
   return (
-    
-      <main className="container">
-        <div className="buttons">
-          <a href={twitter} id="twitter">
-            Twitter Link
-          </a>
-          <a href={zuriBtn} id="btn__zuri">
-            Zuri Team
-          </a>
-          <a href={zuriBooks} id="books">
-            Zuri Books
-          </a>
-          <a href={zuriBooks} id="book__python">
-            Python Books
-          </a>
-          <a href={pitch} id="pitch">
-            Background Check for Coders
-          </a>
-          <a href={zuriDesign} id="book__design">
-            Design Books
-          </a>
-          <Link to="/contact" id="contact">
+    <main className="container">
+      <div className="buttons">
+        <a href={twitter} id="twitter">
+          Twitter Link
+        </a>
+        <a href={zuriBtn} id="btn__zuri">
+          Zuri Team
+        </a>
+        <a href={zuriBooks} id="books">
+          Zuri Books
+        </a>
+        <a href={zuriBooks} id="book__python">
+          Python Books
+        </a>
+        <a href={pitch} id="pitch">
+          Background Check for Coders
+        </a>
+        <a href={zuriDesign} id="book__design">
+          Design Books
+        </a>
+        <Link to="/contact" id="contact">
           contact us
-          </Link>
-        </div>
-        <div className="icons">
-          <img src={slack} alt="slack" />
-          <img src={github} alt="github" />
-        </div>
-      </main>
-
+        </Link>
+      </div>
+      <div className="icons">
+        <img src={slack} alt="slack" />
+        <img src={github} alt="github" />
+      </div>
+    </main>
   );
 }
 
